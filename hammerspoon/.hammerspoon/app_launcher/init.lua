5bd5ec2257c2afe4fbbb75c795593439cd98cbe0
Fython <fang.jia.hui123@gmail.com>
2025-08-15 23:49:50 +0800

    add new shortcuts

--- hammerspoon/.hammerspoon/app_launcher/init.lua
+++ hammerspoon/.hammerspoon/app_launcher/init.lua
@@ -1,18 +1,25 @@
-launcherHotkey = {'option'}
-
+launcherHotkey = { "option" }
 
 applist = {
-    {shortcut = '1',appname = 'Finder'},
-    {shortcut = '2',appname = 'Google Chrome'},
-    {shortcut = '3',appname = '/Applications/WezTerm.app'},
-    {shortcut = '4',appname = 'WeChat'},
-    {shortcut = '5',appname = 'PyCharm'},
+  { shortcut = "1", appname = "Finder" },
+  { shortcut = "2", appname = "Google Chrome" },
+  { shortcut = "3", appname = "/Applications/WezTerm.app" },
 }
 
 hs.fnutils.each(applist, function(entry)
-    hs.hotkey.bind(launcherHotkey, entry.shortcut, entry.appname, function()
-        hs.application.launchOrFocus(entry.appname)
-        hs.alert.closeAll()
-    end)
+  hs.hotkey.bind(launcherHotkey, entry.shortcut, entry.appname, function()
+    hs.application.launchOrFocus(entry.appname)
+    hs.alert.closeAll()
+  end)
 end)
 
+-- use `command+shift+p` open/hide keepassxc
+hs.hotkey.bind({ "cmd", "shift" }, "p", function()
+  local bundleID = "org.keepassxc.keepassxc"
+  local frontmostApp = hs.application.frontmostApplication()
+  if frontmostApp and frontmostApp:bundleID() == bundleID then
+    hs.eventtap.keyStroke({ "cmd" }, "h")
+  else
+    hs.application.launchOrFocusByBundleID(bundleID)
+  end
+end)
