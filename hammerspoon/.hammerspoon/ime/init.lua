4665f51d19b9ba0152d95da9aa197a8660593cd4
Fython <fang.jia.hui123@gmail.com>
2025-09-22 21:34:05 +0800

    update hammerspoon config

--- hammerspoon/.hammerspoon/ime/init.lua
+++ hammerspoon/.hammerspoon/ime/init.lua
@@ -1,81 +1,86 @@
 local function switch2IME(ime)
-    if hs.keycodes.currentSourceID() ~= ime then
-    	hs.keycodes.currentSourceID(ime)
-    end
+  if hs.keycodes.currentSourceID() ~= ime then
+    hs.keycodes.currentSourceID(ime)
+  end
 end
 
 local function Chinese()
-    -- local ime = "com.apple.inputmethod.SCIM.Shuangpin"
-    -- local ime = "com.sogou.inputmethod.sogou.pinyin"
-    -- local ime = "com.apple.inputmethod.SCIM.WBX"  -- 五笔
-    local ime = "im.rime.inputmethod.Squirrel.Hans" -- 鼠鬚管
-    switch2IME(ime)
+  -- local ime = "com.apple.inputmethod.SCIM.Shuangpin"
+  -- local ime = "com.sogou.inputmethod.sogou.pinyin"
+  -- local ime = "com.apple.inputmethod.SCIM.WBX"  -- 五笔
+  local ime = "im.rime.inputmethod.Squirrel.Hans" -- 鼠鬚管
+  switch2IME(ime)
 end
 
 local function English()
-    local ime = "com.apple.keylayout.ABC"
-    switch2IME(ime)
+  local ime = "com.apple.keylayout.ABC"
+  switch2IME(ime)
 end
 
 -- app to expected ime config
 local app2Ime = {
-    {'/Applications/iTerm.app', 'English'},
-    {'/Applications/Hyper.app', 'English'},
-    {'/Applications/Xcode.app', 'English'},
-    {'/Applications/WezTerm.app', 'English'},
-    {'/Applications/Visual Studio Code.app', 'English'},
-    {'/Applications/PyCharm.app', 'English'},
-    {'/Applications/Firefox.app', 'English'},
-    {'/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app', 'English'},
-    {'/Applications/Safari.app', 'English'},
-    {'/Applications/Google Chrome.app', 'English'},
-    {'/System/Library/CoreServices/Finder.app', 'English'},
-    {'/Applications/DingTalk.app', 'Chinese'},
-    {'/Applications/WeChat.app', 'Chinese'},
-    {'/Applications/System Preferences.app', 'English'},
-    {'/Applications/Preview.app', 'Chinese'},
-    {'/Applications/Raycast.app', 'English'},
+  { "/Applications/iTerm.app",                                              "English" },
+  { "/Applications/Hyper.app",                                              "English" },
+  { "/Applications/Xcode.app",                                              "English" },
+  { "/Applications/WezTerm.app",                                            "English" },
+  { "/Applications/Visual Studio Code.app",                                 "English" },
+  { "/Applications/PyCharm.app",                                            "English" },
+  { "/Applications/Firefox.app",                                            "English" },
+  { "/System/Volumes/Preboot/Cryptexes/App/System/Applications/Safari.app", "English" },
+  { "/Applications/Safari.app",                                             "English" },
+  { "/Applications/Google Chrome.app",                                      "English" },
+  { "/System/Library/CoreServices/Finder.app",                              "English" },
+  { "/Applications/DingTalk.app",                                           "Chinese" },
+  { "/Applications/WeChat.app",                                             "Chinese" },
+  { "/Applications/System Preferences.app",                                 "English" },
+  { "/Applications/Preview.app",                                            "Chinese" },
+  { "/Applications/Raycast.app",                                            "English" },
 }
 
-function updateFocusAppInputMethod()
-    local ime = 'English'
-    local focusAppPath = hs.window.frontmostWindow():application():path()
-    for index, app in pairs(app2Ime) do
-        local appPath = app[1]
-        local expectedIme = app[2]
+function updateFocusAppInputMethodForApp(appObject)
+  if not appObject then
+    return
+  end
 
-        if focusAppPath == appPath then
-            ime = expectedIme
-            break
-        end
-    end
+  local ime = "English"
+  local focusAppPath = appObject:path()
 
-    if ime == 'English' then
-        English()
-    else
-        Chinese()
+  for _, app in pairs(app2Ime) do
+    local appPath = app[1]
+    local expectedIme = app[2]
+
+    if focusAppPath == appPath then
+      ime = expectedIme
+      break
     end
+  end
+
+  if ime == "English" then
+    English()
+  else
+    Chinese()
+  end
 end
 
 -- helper hotkey to figure out the app path and name of current focused window
-hs.hotkey.bind({'ctrl', 'cmd'}, ".", function()
-	local data = "App path:        "
-    ..hs.window.focusedWindow():application():path()
-    .."\n"
-    .."App name:      "
-    ..hs.window.focusedWindow():application():name()
-    .."\n"
-    .."IM source id:  "
-    ..hs.keycodes.currentSourceID()
-    print("\n" .. data)
-    hs.alert.show(data)
+hs.hotkey.bind({ "ctrl", "cmd" }, ".", function()
+  local data = "App path:        "
+      .. hs.window.focusedWindow():application():path()
+      .. "\n"
+      .. "App name:      "
+      .. hs.window.focusedWindow():application():name()
+      .. "\n"
+      .. "IM source id:  "
+      .. hs.keycodes.currentSourceID()
+  print("\n" .. data)
+  hs.alert.show(data)
 end)
 
 -- Handle cursor focus and application's screen manage.
 function applicationWatcher(appName, eventType, appObject)
-    if (eventType == hs.application.watcher.activated) then
-        updateFocusAppInputMethod()
-    end
+  if eventType == hs.application.watcher.activated then
+    updateFocusAppInputMethodForApp(appObject)
+  end
 end
 
 appWatcher = hs.application.watcher.new(applicationWatcher)
